2f794255823a22ccac092c21997caa31a0dfa2d0
João Batista <joao-oliveirabatista@hotmail.com>
2023-10-20 13:29:17 +0100

    update styling and fixed bug in the Team/Edit page

---
+++ Views/Shared/_ValidationScriptsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamUserManagementSystem.Views.Shared
+{
+    public class _Validation_ScriptsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
